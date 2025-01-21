8d1dbe8a5377a63a60148d8975804a7292911b01
wakaree <svegliatixanax42@gmail.com>
2025-01-21 23:37:50 +0100

    fix: dolben payment info

--- src/milkpay/cryptomus/business/types/payment_update.py
+++ src/milkpay/cryptomus/business/types/payment_update.py
@@ -11,17 +11,19 @@ class PaymentUpdate(CryptomusUpdate):
     uuid: str
     order_id: str
     amount: StrFloat
-    payment_amount: StrFloat
-    payment_amount_usd: StrFloat
-    merchant_amount: StrFloat
-    commission: StrFloat
+    payment_amount: Optional[StrFloat] = None
+    payment_amount_usd: Optional[StrFloat] = None
+    merchant_amount: Optional[StrFloat] = None
+    commission: Optional[StrFloat] = None
     is_final: bool
     status: str
-    from_address: str = Field(alias="from")
+    from_address: Optional[str] = Field(default=None, alias="from")
     wallet_address_uuid: Optional[str] = None
-    network: str
+    network: Optional[str] = None
     currency: str
     payer_currency: str
+    payer_amount: StrFloat
+    payer_amount_exchange_rate: Optional[StrFloat] = None
     additional_data: Optional[str] = None
     convert: Optional[Convert] = None
     txid: Optional[str] = None
